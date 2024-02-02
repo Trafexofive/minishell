753ae23c445ae829aef133bcf840acacd36ab493
Trafexofive <les5phenix@gmail.com>
2024-02-02 04:15:12 +0100

    last issue

--- parse/tokenizer.c
+++ parse/tokenizer.c
@@ -4,238 +4,203 @@
 
 t_oken **parse(const char *line);
 
-void default_token(t_oken *head)
+bool check_line(char *line, t_info *info) // for checking early parse errors
 {
-	// default values for the token node;
-	if (!head->next || !head->prev)
-	{
-		head->next = NULL;
-		head->prev = NULL;
-	}
-	head->dollar_presence = FALSE;
-	head->quote_type = -1;
-	head->data_type = -1;
+  if (!valid_quotes(info))
+    return FALSE;
+  while (line[info->cursor]) {
+    if (!is_space(line[info->cursor])) 
+      break;
+    info->cursor += 1;
+  }
+  return TRUE;
+} // such ar the '=' at the beggining
+
+void default_token(t_oken *head) {
+  // default values for the token node;
+  if (!head->next || !head->prev) {
+    head->next = NULL;
+    head->prev = NULL;
+  }
+  head->dollar_presence = FALSE;
+  head->quote_type = -1;
+  head->data_type = -1;
 }
 // allocate for the token outside
-t_oken *add_token(char *str_token, t_info *info)
-{
-	size_t size = sizeof(t_oken);
-	t_oken *iter = info->head;
-	if (info->head == NULL)
-	{
-		t_oken *token = chad_alloc(size, 1, info->alloc_head);
-		info->head = token;
-		default_token(token);
-		token->token = str_token;
-		return (token);
-	}
-	else
-	{
-		while (iter->next != NULL)
-		{
-			iter = iter->next;
-		}
-		t_oken *token = chad_alloc(size, 1, info->alloc_head);
-		default_token(token);
-		iter->next = token;
-		token->prev = iter;
-		token->next = NULL;
-		token->token = str_token;
-		return (token);
-	}
+t_oken *add_token(char *str_token, t_info *info) {
+  size_t size = sizeof(t_oken);
+  t_oken *iter = info->head;
+  if (info->head == NULL) {
+    t_oken *token = chad_alloc(size, 1, info->alloc_head);
+    info->head = token;
+    default_token(token);
+    token->token = str_token;
+    return (token);
+  } else {
+    while (iter->next != NULL) {
+      iter = iter->next;
+    }
+    t_oken *token = chad_alloc(size, 1, info->alloc_head);
+    default_token(token);
+    iter->next = token;
+    token->prev = iter;
+    token->next = NULL;
+    token->token = str_token;
+    return (token);
+  }
 }
-
+void handle_operator(char *line, t_info *info) {
+  t_oken *new_token;
+  if (line[info->cursor] == PIPE) {
+    char *str_token = chad_alloc(1, 2, info->alloc_head);
+    str_token[0] = '|';
+    str_token[1] = '\0';
+    new_token = add_token(str_token, info);
+    new_token->data_type = 5;
+    if (line[info->cursor])
+      info->cursor++;
+  } 
+  else if (line[info->cursor] == '>') {
+    if ((line[info->cursor + 1]) == '>') {
+      char *str_token = ft_strdup(">>");
+      add_address(str_token, info->alloc_head);
+      new_token = add_token(str_token, info);
+      new_token->data_type = 4;
+      info->cursor += 2;
+    } else {
+      char *str_token = ft_strdup(">");
+      add_address(str_token, info->alloc_head);
+      new_token = add_token(str_token, info);
+      new_token->data_type = 2;
+      info->cursor += 1;
+    }
+  } else if (line[info->cursor] == '<') {
+    if (line[info->cursor + 1] == '<') {
+      char *str_token = ft_strdup("<<");
+      add_address(str_token, info->alloc_head);
+      new_token = add_token(str_token, info);
+      new_token->data_type = 3;
+      info->cursor += 2;
+    } else {
+      char *str_token = ft_strdup("<");
+      add_address(str_token, info->alloc_head);
+      new_token = add_token(str_token, info);
+      new_token->data_type = 1;
+      info->cursor += 1;
+    }
+  }
+}
 // syntax checking
 void check_syntax(t_oken **tokens);
 
 // tokenizing words and operators
-t_oken *handle_quote(char *line, t_info *info)
-{
-	// handling quotes by taking everything inside them regardless;
-	// update quote type
-	char *str_token;
-	t_oken *new_token;
-	int j = 0;
-	info->cursor += 1;
-	int i = info->cursor;
-	int len = quote_len(line, info) + 1;
-	str_token = chad_alloc(sizeof(char), len, info->alloc_head);
-	while (i < len)
-	{
-		str_token[j] = line[i];
-		j++;
-		i++;
-	}
-	str_token[len] = '\0';
-	new_token = add_token(str_token, info);
-	if (line[info->cursor - 1] == DQUOTE)
-		new_token->quote_type = 1;
-	else
-		new_token->quote_type = 0;
-	info->cursor += len;
-	return (new_token);
+t_oken *handle_quote(char *line, t_info *info) {
+  // handling quotes by taking everything inside them regardless;
+  // update quote type
+  //quotes not working if they are the last token
+  char *str_token;
+  t_oken *new_token;
+  int j = 0;
+  info->cursor += 1;
+  int i = info->cursor;
+  int len = quote_len(line, info);
+  printf("quote len ==> %d\n", len);
+  str_token = chad_alloc(sizeof(char), len, info->alloc_head);
+  while (j < len) {
+    str_token[j] = line[i];
+    j++;
+    i++;
+  }
+  str_token[len] = '\0';
+  new_token = add_token(str_token, info);
+  if (line[info->cursor - 1] == DQUOTE)
+    new_token->quote_type = 1;
+  else
+    new_token->quote_type = 0;
+  new_token->data_type = 6;
+  info->cursor += i;
+  return (new_token);
 }
 
-// return the index of end of quote + 1 aka first occurence of quote;
-
-int keep_track_of_quote(char *line, t_info *info);
-
-bool check_line(char *line, t_info *info) // for checking early parse errors
-{
-	if (!valid_quotes(info))
-		return FALSE;
-	while (line[info->cursor])
-	{
-		if (!is_space(line[info->cursor]))
-			break;
-		info->cursor += 1;
-	}
-	return TRUE;
-} // such ar the '=' at the beggining
-// will also return index of the first word occurence
-
-// maybe for operators tokenize everything (word + op + word)
-
-void handle_operator(char *line, t_info *info)
-{
-	t_oken *new_token;
-	if (line[info->cursor] == PIPE)
-	{
-		char *str_token = chad_alloc(1, 2, info->alloc_head);
-		str_token[0] = '|';
-		str_token[1] = '\0';
-		new_token = add_token(str_token, info);
-		new_token->data_type = 5;
-		info->cursor++;
-	}
-	else if (line[info->cursor] == '>')
-	{
-		if ((line[info->cursor + 1]) == '>')
-		{
-			char *str_token = ft_strdup(">>");
-			add_address(str_token, info->alloc_head);
-			new_token = add_token(str_token, info);
-			new_token->data_type = 4;
-			info->cursor += 2;
-		}
-		else
-		{
-			char *str_token = ft_strdup(">");
-			add_address(str_token, info->alloc_head);
-			new_token = add_token(str_token, info);
-			new_token->data_type = 2;
-			info->cursor += 1;
-		}
-	}
-	else if (line[info->cursor] == '<')
-	{
-		if (line[info->cursor + 1] == '<')
-		{
-			char *str_token = ft_strdup("<<");
-			add_address(str_token, info->alloc_head);
-			new_token = add_token(str_token, info);
-			new_token->data_type = 3;
-			info->cursor += 2;
-		}
-		else
-		{
-			char *str_token = ft_strdup("<");
-			add_address(str_token, info->alloc_head);
-			new_token = add_token(str_token, info);
-			new_token->data_type = 1;
-			info->cursor += 1;
-		}
-	}
+t_oken *handle_word(char *line, t_info *info) {
+  char *str_token;
+  t_oken *new_token;
+  int j = 0;
+  int i = info->cursor;
+  int len = word_len(info);
+  printf("word len ==> %d\n", len);
+  str_token = chad_alloc(sizeof(char), len + 1, info->alloc_head);
+  while (j < len) {
+    str_token[j] = line[i];
+    j++;
+    i++;
+  }
+  str_token[len] = '\0';
+  info->cursor = i;
+  new_token = add_token(str_token, info);
+  new_token->data_type = 6;
+  return (new_token);
 }
 
-t_oken *handle_word(char *line, t_info *info)
-{
-	char *str_token;
-	t_oken *new_token;
-	int j = 0;
-	int i = info->cursor;
-	int len = word_len(info);
-	printf("word len ==> %d\n", len);
-	str_token = chad_alloc(sizeof(char), len, info->alloc_head);
-	while (j < len)
-	{
-		str_token[j] = line[i];
-		j++;
-		i++;
-	}
-	str_token[len] = '\0';
-	info->cursor = i;
-	new_token = add_token(str_token, info);
-	new_token->data_type = 6;
-	return (new_token);
+void handle_dollar(char *line, t_info *info) {
+  t_oken *new_token = handle_word(line, info);
+  new_token->dollar_presence = TRUE;
 }
 
-void handle_dollar(char *line, t_info *info)
-{
-	t_oken *new_token = handle_word(line, info);
-	new_token->dollar_presence = TRUE;
-}
+t_info *main_loop(char *line, t_info *info) {
 
-t_info *main_loop(char *line, t_info *info)
-{
+  if (!check_line(line, info))
+    return NULL;
+  while (line[info->cursor]) {
+    if (line[info->cursor] == DQUOTE || line[info->cursor] == QUOTE)
+    {
+      handle_quote(line, info);
+    }
+    else if (is_operator(line[info->cursor]))
+      handle_operator(line, info);
+    else if (ft_isprint(line[info->cursor]) && !is_space(line[info->cursor]) && !is_operator(line[info->cursor]) && !is_quote(line[info->cursor])){
+      handle_word(line, info);
+    } else if (line[info->cursor] == '$')
+      handle_dollar(line, info);
+    else if (is_space(line[info->cursor]))
+      info->cursor++;
 
-	if (!check_line(line, info))
-		return NULL;
-	while (line[info->cursor])
-	{
-		if (line[info->cursor] == DQUOTE || line[info->cursor] == QUOTE)
-			handle_quote(line, info);
-		else if (is_operator(line[info->cursor]))
-			handle_operator(line, info);
-		else if (ft_isprint(line[info->cursor]) && !is_space(line[info->cursor]))
-		{
-			handle_word(line, info);
-		}
-		else if (line[info->cursor] == '$')
-			handle_dollar(line, info);
-		if (!line[info->cursor])
-			break;
-
-		info->cursor++;
-	}
-	return (info);
+  }
+  return (info);
 }
 
 // bool is_in(char c, const char *str);
 
-void	print_cmd(t_cmd *cmd)
-{
-	int i = 0;
-	while (cmd->cmd[i] != NULL)
-	{
-		printf("%s\n", cmd->cmd[i]);
-		i++;
-	}
+void print_cmd(t_cmd *cmd) {
+  int i = 0;
+  while (cmd->cmd[i] != NULL) {
+    printf("%s\n", cmd->cmd[i]);
+    i++;
+  }
 }
 
+int main(void) {
+  t_info *info;
+  char *line;
+  t_alloc *alloc_head = ft_calloc(1, sizeof(t_alloc));
 
-int main(void)
-{
-	t_info *info;
-	char *line;
-	t_alloc *alloc_head = ft_calloc(1, sizeof(t_alloc));
+  alloc_head->next = NULL;
+  info = ft_calloc(1, sizeof(t_info));
+  alloc_head->address = info;
+  // line = ft_strdup("ls -la >> << > < $hello | grep a | tr a b | cat -e");
+  line = readline("minishell$ ");
+  info->line = line;
+  info->alloc_head = alloc_head;
+  info->head = NULL;
+  info->cursor = 0;
 
-	alloc_head->next = NULL;
-	info = ft_calloc(1, sizeof(t_info));
-	alloc_head->address = info;
-	line = ft_strdup("ls -la  $hello | grep a | tr a b | cat -e");
-	info->line = line;
-	info->alloc_head = alloc_head;
-	info->head = NULL;
-	info->cursor = 0;
+  //   add_address(line, alloc_head);
+  main_loop(line, info);
+  print_tokens(info->head);
+  //lexer(info);
+  // print_cmd(info->cmd);
+  free(line);
+  free_all(alloc_head);
 
-	//   add_address(line, alloc_head);
-	main_loop(line, info);
-	lexer(info);
-	print_cmd(info->cmd);
-	// print_tokens(info->head);
-	free(line);
-	free_all(alloc_head);
-
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
--- parse/utils.c
+++ parse/utils.c
@@ -2,141 +2,140 @@
 
 #include "../inc/minish.h"
 
-void	parse_error(const char *exit_msg, t_info *info)
-{
-	printf("%s\n", exit_msg);
-	free_all(info->alloc_head);
-	exit(1);
+void parse_error(const char *exit_msg, t_info *info) {
+  printf("%s\n", exit_msg);
+  free_all(info->alloc_head);
+  exit(1);
 }
 
-void	print_arr(char **str)
-{
-	int	i;
+void print_arr(char **str) {
+  int i;
 
-	i = -1;
-	while (str[++i])
-		printf("%s\n", str[i]);
+  i = -1;
+  while (str[++i])
+    printf("%s\n", str[i]);
 }
 
-bool	is_space(char c)
-{
-	if (c == ' ' || c == '\t')
-		return (TRUE);
-	return (FALSE);
+bool is_space(char c) {
+  if (c == ' ' || c == '\t')
+    return (TRUE);
+  return (FALSE);
 }
-int	quote_len(char *line, t_info *info)
-{
-	int	i;
-	int	j;
+int quote_len(char *line, t_info *info) {
+  int i;
+  int j;
 
-	i = info->cursor;
-	j = 0;
-	while (line[i])
-	{
-		if (line[i] == 34 || line[i] == 39)
-			return (j);
-		i++;
-		j++;
-	}
-	return (-1);
-}
+  i = info->cursor;
+  j = 0;
+  if (line[i - 1] == 34)
+  {
+    while (line[i])
+    {
+      if (line[i] == 34)
+        return (j);
+      i++;
+      j++;
+    }
+  }
+  else if (line[i - 1] == 39)
+  {
+  while (line[i]) {
+    if (line[i] == 39)
+      return (j);
+    i++;
+    j++;
+  }
 
-bool	is_operator(char c)
-{
-	if (c == PIPE || c == '>' || c == '<')
-		return (TRUE);
-	return (FALSE);
-}
-
-bool	is_quote(char c)
-{
-	if (c == DQUOTE || c == QUOTE)
-		return (TRUE);
-	return (FALSE);
+  }
+  return (-1);
 }
 
-int	last_char_in_word(char *line, t_info *info)
-{
-	int	i;
-
-	i = info->cursor;
-	while (line[i] != '\0' && !is_space(line[i]) && !is_quote(line[i]))
-		i++;
-	i--;
-	return (i);
+bool is_operator(char c) {
+  if (c == PIPE || c == '>' || c == '<')
+    return (TRUE);
+  return (FALSE);
 }
 
-int	word_len(t_info *info)
-{
-	int	i;
-	int	j;
-
-	i = info->cursor;
-	j = 0;
-	while (info->line[i] != '\0' && !is_space(info->line[i])
-		&& !is_quote(info->line[i]))
-	{
-		i++;
-		j++;
-	}
-	return (j);
-}
-bool	valid_quotes(t_info *info)
-{
-	char	*line;
-	int		i;
-	int		quote_c;
-	int		dquote_c;
-
-	line = info->line;
-	i = 0;
-	quote_c = 0;
-	dquote_c = 0;
-	while (line[i])
-	{
-		if (line[i] == DQUOTE)
-			dquote_c++;
-		if (line[i] == DQUOTE)
-			quote_c++;
-		i++;
-	}
-	if (dquote_c % 2 != 0 && dquote_c != 0)
-	{
-		puts("invalid quotes");
-		parse_error("invalid quotes", info);
-	}
-	if (quote_c % 2 != 0 && quote_c != 0)
-	{
-		puts("invalid quotes");
-		parse_error("invalid quotes", info);
-	}
-	return (TRUE);
+bool is_quote(char c) {
+  if (c == DQUOTE || c == QUOTE)
+    return (TRUE);
+  return (FALSE);
 }
 
-const char *translate(int c)
-{
-	if (c == 1)
-		return ("REDIR_IN");
-	if (c == 2)
-		return ("REDIR_OUT");
-	if (c == 3)
-		return ("heredoc");
-	if (c == 4)
-		return ("herestring");
-	if (c == 5)
-		return ("PIPE");	
-	if (c == 6)	
-		return ("WORD");
-	return NULL;
+int last_char_in_word(char *line, t_info *info) {
+  int i;
+
+  i = info->cursor;
+  while (line[i] != '\0' && !is_space(line[i]) && !is_quote(line[i]))
+    i++;
+  i--;
+  return (i);
 }
 
-void	print_tokens(t_oken *head_token)
-{
-	t_oken *ptr = head_token;
-		while (ptr->next != NULL)
-		{
-			printf("token :%s--quote value =>%d--type =>%s \n", ptr->token, ptr->quote_type, translate(ptr->data_type));
-			ptr = ptr->next;
-		}
-		printf("token => %s", ptr->token);
+int word_len(t_info *info) {
+  int i;
+  int j;
+
+  i = info->cursor;
+  j = 0;
+  while (info->line[i] != '\0' && !is_space(info->line[i]) &&
+         !is_quote(info->line[i]) && !is_operator(info->line[i])){
+    i++;
+    j++;
+  }
+  return (j);
+}
+bool valid_quotes(t_info *info) {
+  char *line;
+  int i;
+  int quote_c;
+  int dquote_c;
+
+  line = info->line;
+  i = 0;
+  quote_c = 0;
+  dquote_c = 0;
+  while (line[i]) {
+    if (line[i] == DQUOTE)
+      dquote_c++;
+    if (line[i] == DQUOTE)
+      quote_c++;
+    i++;
+  }
+  if (dquote_c % 2 != 0 && dquote_c != 0) {
+    puts("invalid quotes");
+    parse_error("invalid quotes", info);
+  }
+  if (quote_c % 2 != 0 && quote_c != 0) {
+    puts("invalid quotes");
+    parse_error("invalid quotes", info);
+  }
+  return (TRUE);
+}
+
+const char *translate(int c) {
+  if (c == 1)
+    return ("REDIR_IN");
+  if (c == 2)
+    return ("REDIR_OUT");
+  if (c == 3)
+    return ("heredoc");
+  if (c == 4)
+    return ("herestring");
+  if (c == 5)
+    return ("PIPE");
+  if (c == 6)
+    return ("WORD");
+  return NULL;
+}
+
+void print_tokens(t_oken *head_token) {
+  t_oken *ptr = head_token;
+  while (ptr->next != NULL) {
+    printf("token :%s--quote value =>%d--type =>%s \n", ptr->token,
+           ptr->quote_type, translate(ptr->data_type));
+    ptr = ptr->next;
+  }
+    printf("token :%s--quote value =>%d--type =>%s \n", ptr->token,
+           ptr->quote_type, translate(ptr->data_type));
 }
